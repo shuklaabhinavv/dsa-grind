5148c7dd5331998cec11de9c5ddf1e98480a15c2
Abhinav Shukla <abhinav24shukla08@gmail.com>
2024-08-14 08:39:52 +0530

    Time: 8 ms (63.77%), Space: 9.8 MB (30.14%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char,int>m;
+        if(s.size()!=t.size()) return false;
+        for(int i=0;i<s.size();i++){
+            m[s[i]]++;
+        }
+        for(int j=0;j<t.size();j++){
+            if(m[t[j]]==0){
+                return false;
+            }
+            else{
+                m[t[j]]--;
+            }
+        }
+        return true;
+    }
+};
