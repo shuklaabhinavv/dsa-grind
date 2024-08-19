bfd0f936fe39df268455194e18197b070fd19ded
Abhinav Shukla <abhinav24shukla08@gmail.com>
2024-08-19 21:24:58 +0530

    Time: 40 ms (9.15%), Space: 155.7 MB (15.26%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
@@ -1,21 +1,31 @@
 class Solution {
 public:
+    int findways(int num, int i, int tar, vector<vector<int>> &dp) 
+    {
+        if (num == tar) 
+        {
+            return 0;
+        }
+        if (num + i> tar) 
+        {
+            return 2000;
+        }
+        if (dp[num][i] != -1) 
+        {
+            return dp[num][i];
+        }
+        int cp = 2 + findways(num + num, num, tar, dp);
+        int p = 1 + findways(num + i,i, tar, dp);
 
-    int f(int num, int lastcp, int tar, vector<vector<int>> &dp) {
-    if (num == tar) return 0;
-    if (num + lastcp > tar) return 2000;
-    
-    if (dp[num][lastcp] != -1) return dp[num][lastcp];
-    int cp = 2 + f(num + num, num, tar, dp);
-    int p = 1 + f(num + lastcp, lastcp, tar, dp);
-
-    return dp[num][lastcp] = min(cp, p);
+        return dp[num][i] = min(cp, p);
     }
-
-    int minSteps(int n) {
-        if (n == 1) return 0;
+    int minSteps(int n) 
+    {
+        if (n == 1)
+        {
+            return 0;
+        }
         vector<vector<int>> dp(n + 1, vector<int>(n, -1));
-        return f(2, 1, n, dp) + 2;
+        return findways(2, 1, n, dp) + 2;
     }
-
 };
